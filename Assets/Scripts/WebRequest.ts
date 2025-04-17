cbc0e52c82873d538aa91c9eb0f5579ca0451e74
Advay Patil <advaypatil27@gmail.com>
2025-04-17 15:05:25 -0700

    Fetch builder pattern

--- Assets/Scripts/WebRequest.ts
+++ Assets/Scripts/WebRequest.ts
@@ -1,20 +1,21 @@
+import { Fetch } from './Fetch';
+
 @component
 export class WebRequest extends BaseScriptComponent {
   @input remoteServiceModule: RemoteServiceModule;
 
   public makeAPICall(b: boolean) {
-    const request = RemoteServiceHttpRequest.create();
-    request.method = RemoteServiceHttpRequest.HttpRequestMethod.Get;
-    request.url = 'https://catfact.ninja/fact';
-
-    print(request.url);
-
-    this.remoteServiceModule.performHttpRequest(request, (response) => {
-      print(`HTTP CODE ${response.statusCode}`);
-      print(`Content-Type: ${response.contentType}`);
-      print(`Headers: ${JSON.stringify(response.headers)}`);
-      print(response.body);
-      print(request.url);
-    });
+    new Fetch(this.remoteServiceModule)
+      .method(RemoteServiceHttpRequest.HttpRequestMethod.Get)
+      .url('https://catfact.ninja/fact')
+      .body({})
+      .perform(this.handler);
   }
+
+  handler: (response: RemoteServiceHttpResponse) => void = (response) => {
+    print(`HTTP CODE ${response.statusCode}`);
+    print(`Content-Type: ${response.contentType}`);
+    print(`Headers: ${JSON.stringify(response.headers)}`);
+    print(response.body);
+  };
 }
