3728ff33528ef6b39d94b7d0ec7fc4a8a2180a8f
Advay Patil <advaypatil27@gmail.com>
2025-05-02 11:01:16 -0700

    Add speech controller

---
+++ Assets/Scripts/Utils/SpeechController.ts
@@ -0,0 +1,30 @@
+@component
+export class SpeechToText extends BaseScriptComponent {
+  private readonly voiceMlModule =
+    require('LensStudio:VoiceMLModule') as VoiceMLModule;
+
+  onAwake() {
+    this.createEvent('OnStartEvent').bind(() => this.onStart());
+  }
+
+  onStart() {
+    const options = VoiceML.ListeningOptions.create();
+    options.shouldReturnInterimAsrTranscription = false;
+    options.shouldReturnAsrTranscription = true;
+
+    this.voiceMlModule.onListeningEnabled.add(() => {
+      this.voiceMlModule.startListening(options);
+    });
+    this.voiceMlModule.onListeningDisabled.add(() => {
+      this.voiceMlModule.stopListening();
+    });
+    this.voiceMlModule.onListeningError.add((event) => {
+      print(`${event.error}: ${event.description}`);
+    });
+    this.voiceMlModule.onListeningUpdate.add((event) => {
+      if (event.transcription) {
+        print(event.transcription);
+      }
+    });
+  }
+}
