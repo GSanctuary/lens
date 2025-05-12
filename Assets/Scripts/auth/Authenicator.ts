53e44447bf162c96ea88c79adb5e95b5af7a7c05
Advay Patil <advaypatil27@gmail.com>
2025-05-11 17:01:32 -0700

    Create API key if not found

--- Assets/Scripts/auth/Authenicator.ts
+++ Assets/Scripts/auth/Authenicator.ts
@@ -1,3 +1,4 @@
+import { SanctuaryAPI } from "../services/SanctuaryAPI";
 import { PersistentStorageManager } from "../utils/PersistentStorageManager";
 
 @component
@@ -7,17 +8,24 @@ export class Authenticator extends BaseScriptComponent {
     authKey: string;
 
     onAwake() {
-        this.createEvent("OnStartEvent").bind(() => {
+        this.createEvent("OnStartEvent").bind(async () => {
             const store = PersistentStorageManager.getInstance();
             const existingAPIKey = store.get(this.authKey);
             if (existingAPIKey) {
                 print(`Found API Key: ${existingAPIKey}`);
             } else {
-                print("Setting API key...");
-                store.set(this.authKey, "test");
-            }
+                print(`No API Key found. Generating a new one.`);
+                const newAPIKey = await this.generateAPIKey();
+                store.set(this.authKey, newAPIKey);
+                print(`Generated API Key: ${newAPIKey}`);
 
-            // TODO: Tell event emitter to start listening
+                // TODO: Tell event emitter to start listening
+            }
         });
+    }
+
+    private async generateAPIKey(): Promise<string> {
+        const sanctuaryAPI = SanctuaryAPI.getInstance();
+        return await sanctuaryAPI.getCredentials()
     }
 }
