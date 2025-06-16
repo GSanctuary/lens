c4b3494a49069cfd37b880db059f8221855479af
Advay Patil <advaypatil27@gmail.com>
2025-06-16 16:19:08 -0700

    Add types for current weather route

--- Assets/Scripts/types/Sanctuary.ts
+++ Assets/Scripts/types/Sanctuary.ts
@@ -107,3 +107,25 @@ export const convertRawStickyNoteResponseToStickyNote = (
         updatedAt: new Date(rawStickyNoteResponse.updatedAt),
     };
 };
+
+type Location = {
+    name: string;
+    country: string;
+    lat: number;
+    lon: number;
+};
+
+export type CurrentWeather = {
+    location: Location;
+    current: {
+        temp_c: number;
+        condition: {
+            text: string;
+            icon: string;
+        };
+        humidity: number;
+        wind_kph: number;
+        feelslike_c: number;
+        feelslike_f: number;
+    };
+};
